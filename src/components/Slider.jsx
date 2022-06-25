b6be492b6e71ab851342a50b1ffbe0d293258388
Maryam Faraji <mfaraji00@mylangara.ca>
2022-06-24 22:11:10 -0700

    slider initial commit with bugs

---
+++ src/components/Slider.jsx
@@ -0,0 +1,122 @@
+import styled from "styled-components"
+import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
+import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+import { useState } from "react";
+import { sliderItems } from "../data";
+
+
+const Container = styled.div`
+    width:100%;
+    height:100vh;
+    display: flex;
+    background-color: grey;
+    position: relative;
+    overflow: hidden;
+`
+
+const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #f9f1f1;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top:0px;
+    bottom: 0;
+    left: ${props =>props.direction === "left" && "10px"};
+    right: ${props =>props.direction === "right" && "10px"};
+    margin: auto;
+    cursor: pointer;
+    opacity: 0.6;
+    z-index: 2;
+`
+
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    transition: all 1.5s ease;
+    transform:translateX(${props=>props.slideIndex * -100}vw);
+`
+
+const Slide = styled.div`
+    width: 100vw;
+    height: 100v;
+    display:flex;
+    align-items:center;
+`
+
+const ImgContainer = styled.div`
+    height: 100%;
+    flex:1;
+`
+
+const Image = styled.img`
+    height: 80%;
+`
+
+const InfoContainer = styled.div`
+    flex:1;
+    padding: 50px;
+`
+
+const Title = styled.h1`
+    font-size: 70px;
+`
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`
+
+
+const Slider = () => {
+
+    const[slideIndex,setSlideIndex]= useState(0);
+    
+    const handleClick = (direction) =>{
+        if(direction === "left"){
+            setSlideIndex(slideIndex > 0 ? slideIndex-1 :2 )
+        }else{
+            setSlideIndex(slideIndex < 2 ? slideIndex+1 :0 )
+        }
+    }
+
+  return (
+    <Container>
+        <Arrow direction="left" onClick={handleClick("left")}>
+            <ArrowLeftOutlinedIcon />
+        </Arrow>
+        <Wrapper slideIndex={slideIndex}>
+
+            {sliderItems.map((items)=>(
+                    <Slide bg={items.bg} key={items.id}>
+                        <ImgContainer>
+                            <Image src={items.img}></Image>
+                        </ImgContainer>
+                        <InfoContainer> 
+                            <Title>{items.title}</Title>
+                            <Desc>{items.desc}</Desc>
+                            <Button>Shop Now</Button>
+                        </InfoContainer>
+                    </Slide>
+            ))}
+
+
+        </Wrapper>
+        <Arrow direction="right" onClick={handleClick("right")}>
+            <ArrowRightOutlinedIcon/>
+        </Arrow>
+    </Container>
+  )
+}
+
+export default Slider
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id:1,
+        img:"https://i.ibb.co/XsdmR2c/1.png",
+        title:"Summer Sale",
+        desc: "summer test",
+        bg:"f5fafd"
+    },
+    {
+        id:2,
+        img:"https://i.ibb.co/DG69bQ4/2.png",
+        title:"Winter Sale",
+        desc: "winter test",
+        bg:"fcf1ed"
+    },
+    {
+        id:3,
+        img:"https://i.ibb.co/XsdmR2c/1.png",
+        title:"Clearance Sale",
+        desc: "clearance test",
+        bg:"f5fafd"
+    }
+]
