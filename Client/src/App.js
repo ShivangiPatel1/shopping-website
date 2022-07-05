083899741b62b6574a18648bc172ea0eae436101
Shivangi Patel <shivangi9170@gmail.com>
2022-07-05 15:04:15 -0400

    backnd initial files added

---
+++ Client/src/App.js
@@ -0,0 +1,20 @@
+// import AllProduct from './pages/AllProduct';
+import Home from './pages/Home';
+// import SingleProduct from './pages/SingleProduct';
+// import Register from './pages/Register';
+// import Login from './pages/Login';
+import Cart from './pages/Cart'
+
+
+function App() {
+  return (
+    // <Home/>
+    // <AllProduct/>
+    //<SingleProduct/>
+   //  <Register/>
+   // <Login/>
+    <Cart/>
+  ); 
+}
+
+export default App;
