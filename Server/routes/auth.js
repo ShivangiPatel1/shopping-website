3e6b330ac16289319d6adfa68254a2538ca04b64
Shivangi Patel <shivangi9170@gmail.com>
2022-07-08 22:28:35 -0400

    login-register api implemented

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -3,8 +3,8 @@ const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const dotenv = require("dotenv");
 dotenv.config();
+const jwt = require("jsonwebtoken")
 //Register
-
 router.post("/register", async (req, res) => {
     var encryptPassword = CryptoJS.AES.encrypt(
         req.body.password,
@@ -26,8 +26,27 @@ router.post("/register", async (req, res) => {
 //Login 
 
 router.post("/login",(req,res)=>{
-    const user = await User.findOne({username:req.body.username})
-
-    const hashedPassword = CryptoJS.AES.decrypt()
+    try{
+        const user = await User.findOne({username:req.body.username})
+        !user && res.status(401).json("Wrong Credentials!")
+    const hashedPassword = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.PASS_SEC
+    );
+    const accessToken = jwt.sign({
+        id:user._id,
+        isAdmin:user.isAdmin,
+    },
+    process.env.JWT_SEC,
+    {
+        expiresIn:"3d"
+    })
+    const EnteredPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+    EnteredPassword !== req.body.password && res.body.password && res.status(401).json("Wrong Credentials!");
+    const {password,...otherDetails}=user._doc;
+    res.status(200).json(...otherDetails)    
+}catch (err) {
+        res.status(500).json(err)
+    }
 })
 module.exports = router;
