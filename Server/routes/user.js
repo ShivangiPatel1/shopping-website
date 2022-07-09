7f31f96f85c550b42d686de5379d122bb8133676
Shivangi Patel <shivangi9170@gmail.com>
2022-07-09 16:03:44 -0400

    jwt implementation under progress

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,5 +1,13 @@
+const {verifyToken} = require("./verifyToken")
 const router = require("express").Router();
  
-
+router.put(":/id",verifyToken,(req,res)=>{
+if(req.user.id==req.params.id || req.user.isAdmin){
+    next()
+}
+else {
+    res.status(403).json("You are not alowed to do that!");
+  }
+})
 
 module.exports = router
