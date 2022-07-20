b36c87593f8eb86addd713592db27aaa6923264d
Shivangi Patel <shivangi9170@gmail.com>
2022-07-20 14:34:10 -0700

    update on branch

--- Client/src/Components/ProductsSection.js
+++ Client/src/Components/ProductsSection.js
@@ -1,38 +1,42 @@
-import React ,{useState,useEffect} from 'react'
-import styled from "styled-components"
-import {popularProducts} from "../data"
-import ProductCards from './ProductCards'
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { popularProducts } from "../data";
+import ProductCards from "./ProductCards";
+import axios from "axios";
 
 const Container = styled.div`
-padding:20px;
-display:flex;
-flex-wrap: wrap;
-justify-content: space-between;`
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
 
-const ProductsSection = ({cat,filters,sort}) => {
-  const [products,setProducts]=useState([]);
-  const [filteredProducts,setFilteredProducts]=useState([])
+const ProductsSection = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   useEffect(() => {
-    
-  const getProducts = async ()=>{
-    try{
-  const res = await axios.get(cat ? `http://localhost:8080/api/products?category=${cat}`:"http://localhost:8080/api/products")
-  console.log(res)
-    } catch(e){
-console.log(e)
-    }
-  }
-    getProducts()
-  }, [])
-  
-  return (
-   <Container>
-    {popularProducts.map((item)=>
-<ProductCards item={item} key={item.id}></ProductCards>
-    )}
-   </Container>
-  )
-}
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:8080/api/products?category=${cat}`
+            : "http://localhost:8080/api/products"
+        );
+      setProducts(res.data)
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getProducts();
+  }, []);
 
-export default ProductsSection
+  return (
+    <Container>
+      {popularProducts.map((item) => (
+        <ProductCards item={item} key={item.id}></ProductCards>
+      ))}
+    </Container>
+  );
+};
+
+export default ProductsSection;
