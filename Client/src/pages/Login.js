6e77a2453dbcfb72e7514a4c8f72e98006eaf83b
Maryam Faraji <mfaraji00@mylangara.ca>
2022-07-21 17:57:09 -0700

    redux authentication done

--- Client/src/pages/Login.js
+++ Client/src/pages/Login.js
@@ -2,6 +2,9 @@
 
 import styled from "styled-components"
 import { mobile } from "../responsive"
+import { useState } from "react";
+import { login } from "../redux/apiCalls";
+import { useDispatch, useSelector } from "react-redux";
 
 const Container = styled.div`
   width: 100vw;
@@ -44,6 +47,14 @@ const Button = styled.button`
   color: white;
   cursor: pointer;
   margin-bottom: 10px;
+  &:disabled{
+    color: green;
+    cursor: not-allowed;
+  }
+`;
+
+const Error = styled.span`
+  color:red;
 `;
 
 const Link = styled.a`
@@ -54,14 +65,25 @@ const Link = styled.a`
 `;
 
 const Login = () => {
+
+  const [username,setUsername] = useState("")
+  const [password,setPassword] = useState("")
+  const dispatch = useDispatch();
+  const {isFetching,error} = useSelector((state)=>state.user);
+
+  const handleClick = (e)=>{
+    e.preventDefault();
+    login(dispatch,{username,password});
+  }
   return (
     <Container>
       <Wrapper>
         <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="username" />
-          <Input placeholder="password" />
-          <Button>LOGIN</Button>
+          <Input placeholder="username" onChange={(e)=>setUsername(e.target.value)}/>
+          <Input placeholder="password" onChange={(e)=>setPassword(e.target.value)} type="password"/>
+          <Button onClick={handleClick} disabled={isFetching}>LOGIN</Button>
+          {error && <Error> Something went wrong</Error>}
           <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
           <Link>CREATE A NEW ACCOUNT</Link>
         </Form>
