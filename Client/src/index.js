a6c8245330043771f80ecac0c6f1595e5154fa92
Maryam Faraji <mfaraji00@mylangara.ca>
2022-07-23 15:00:20 -0700

    persist

--- Client/src/index.js
+++ Client/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import {store,persistor} from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+          <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
